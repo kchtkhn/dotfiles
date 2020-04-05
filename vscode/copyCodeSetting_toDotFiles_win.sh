74237cef1a9ad721658306be50359e710707b89c
keiichi <keiichi.takehana@gmail.com>
2020-04-05 17:37:27 +0900

    ctrl+n and ctrl+p

---
+++ vscode/copyCodeSetting_toDotFiles_win.sh
@@ -0,0 +1,2 @@
+cp /mnt/c/Users/keiichi/AppData/Roaming/Code/User/settings.json ~/dotfiles/vscode/
+cp /mnt/c/Users/keiichi/AppData/Roaming/Code/User/keybindings.json ~/dotfiles/vscode/
--- vscode/copyCodeSetting_win.sh
+++
@@ -1,2 +0,0 @@
-cp ~/dotfiles/vscode/settings.json /mnt/c/Users/keiichi/AppData/Roaming/Code/User/
-cp ~/dotfiles/vscode/keybindings.json /mnt/c/Users/keiichi/AppData/Roaming/Code/User/
